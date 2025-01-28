14cd44f815d7955590dd23d277e91e1f257224b0
vhraposo <victorhcraposo@gmail.com>
2025-01-27 22:54:03 -0300

    Aula 18

--- src/main/java/com/rocketseat/service/notification/NotificationController.java
+++ src/main/java/com/rocketseat/service/notification/NotificationController.java
@@ -1,6 +1,7 @@
 package com.rocketseat.service.notification;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/notification")
 public class NotificationController {
 
+    @PostMapping
     public ResponseEntity<Void> sendNotification(@RequestBody NotificationRequest request){
+        System.out.println(request.message());
         return ResponseEntity.ok().build();
     }
     
